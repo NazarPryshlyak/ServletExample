9109698b00c3924c79cb94fd2b9adcfb0692caea
Nazar Pryshlyak <nazar.pryshlyak@gmail.com>
2020-03-09 18:58:27 +0200

    init structure

---
+++ src/main/Java/ua/lviv/lgs/RegistrationServlet.java
@@ -0,0 +1,28 @@
+package ua.lviv.lgs;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+
+    private UserService userService = UserService.getUserService();
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        userService.saveUser(new User(firstName, lastName, email, password));
+        request.setAttribute("userEmail", email);
+        request.getRequestDispatcher("cabinet.jsp").forward(request, response);
+    }
+}
